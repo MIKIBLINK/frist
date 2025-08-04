eccb0cf01c87fccb81dd8ac962154e32730c9885
MIKIBLINK <159888820+MIKIBLINK@users.noreply.github.com>
2025-08-05 01:09:18 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer class="bg-dark text-white text-center py-3 mt-5">
     <div class="container">
-        <p class="mb-0"><strong>About me</strong></p>
+        <p class="mb-0"><strong>Frontend by Seavlinh @ 2026</strong></p>
         <p class="mb-0">Backend Developer</p>
         <p class="mb-0">Email: <a href="mailto:beangchanchakriya@gmail.com"
                 class="text-white">beangchanchakriya@gmail.com</a></p>
@@ -8,4 +8,5 @@
         <p class="mb-0">Telegram contact: <a href="https://t.me/b_chanchakriya" class="text-white"
                 target="_blank">@b_chanchakriya</a></p>
     </div>
+
 </footer>
