9b15774df6b90b18291bd227988cb980479a07e0
MIKIBLINK <159888820+MIKIBLINK@users.noreply.github.com>
2025-08-05 22:10:06 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="bg-dark text-white text-center py-3 mt-5">
     <div class="container">
-        <p>This is just DEMO PROJECT NO REAL TIME !!!
-        <p class="mb-0"><strong>Frontend by Seavlinh @ 2026</strong></p>
+        <p class="mb-0"><strong>This is just project Demo Practice !!Not Real !!</strong></p>
+        <p class="mb-0"><strong>This peoject frontend form Seavlinh</strong></p>
         <p class="mb-0">Backend Developer</p>
         <p class="mb-0">Email: <a href="mailto:beangchanchakriya@gmail.com"
                 class="text-white">beangchanchakriya@gmail.com</a></p>
@@ -9,6 +9,4 @@
         <p class="mb-0">Telegram contact: <a href="https://t.me/b_chanchakriya" class="text-white"
                 target="_blank">@b_chanchakriya</a></p>
     </div>
-
 </footer>
-
